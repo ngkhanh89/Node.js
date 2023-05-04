9aca52f58df63d992affcf86993aaa07d570fb1d
ngkhanh89 <132373289+ngkhanh89@users.noreply.github.com>
2023-05-04 13:35:34 +0700

    Update rinne2.ps1

--- rinne2.ps1
+++ rinne2.ps1
@@ -5,7 +5,7 @@ choco feature enable -n allowGlobalConfirmation
 choco install chrome-remote-desktop-host 
 
 
-DÁN MÃ WINDOWS POWERSHELL CỦA BẠN Ở ĐÂY --pin=123123
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AbUR2VPPNvvFCawT7OXbQ6MG8RXSLsWHuaqxlQU4lQZ2PMJtKgfI_5XR3H1tx3cEReFRXQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123123
 
 write-host Mã pin của bạn là: 123123
 
