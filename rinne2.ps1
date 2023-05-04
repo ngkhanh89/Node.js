9e81823e79a4834e68e9005f0bc382a86d6983aa
nowadashi <132260359+nowadashi@users.noreply.github.com>
2023-05-04 07:20:12 +0700

    Update rinne2.ps1

--- rinne2.ps1
+++ rinne2.ps1
@@ -2,7 +2,11 @@ net user tên người dùng mật khẩu /add
 net localgroup administrators tên người dùng /add
 net.exe user $env:UserName mật khẩu 
 choco feature enable -n allowGlobalConfirmation
-choco install ngrok
-ngrok authtoken token của bạn
-bash ngrok.sh
+choco install chrome-remote-desktop-host 
+
+
+DÁN MÃ WINDOWS POWERSHELL CỦA BẠN Ở ĐÂY --pin=123123
+
+write-host Mã pin của bạn là: 123123
+
 ping -t localhost
