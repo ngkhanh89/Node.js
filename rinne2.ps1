8d9026fab1e4ceda3fedbbd59dc5f924edfaec5d
nowadashi <132260359+nowadashi@users.noreply.github.com>
2023-05-04 07:08:12 +0700

    Create rinne2.ps1

---
+++ rinne2.ps1
@@ -0,0 +1,8 @@
+net user tên người dùng mật khẩu /add
+net localgroup administrators tên người dùng /add
+net.exe user $env:UserName mật khẩu 
+choco feature enable -n allowGlobalConfirmation
+choco install ngrok
+ngrok authtoken token của bạn
+bash ngrok.sh
+ping -t localhost
